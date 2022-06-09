36fdd265f79020936b8df6d152d5231105f2a04a
YaffDa <matthias.weyrich@gmx.de>
2022-06-09 12:51:35 +0200

    check age and joining year

--- login.js
+++ login.js
@@ -17,6 +17,35 @@ function checkFailed() {
     }
 }
 
+function checkAge() {
+    const dob = document.getElementById("DOB").value
+    const date = new Date(dob)
+    const currentDate = Date.now()
+    const age = (currentDate - date) / (31557600000)
+    const join = checkJoin()
+    if(!join) {
+        alert("Invalid joining year")
+    } 
+    else if(age < 17 || age > 60) {
+        alert("Invalid DOB")
+    } else {
+        window.location.href = "students.html";
+    }   
+}
+
+function checkJoin() {
+    const join = document.getElementById("joiningDate").value
+    const joinDate = new Date(join)
+    joinYear = joinDate.toISOString().split('-')[0]
+
+    if(joinYear != 2015) {
+        return false
+    } else {
+        return true
+    }
+
+}
+
 function checkStaffCred() {
     const staffPassword = document.getElementById("stPassword").value
     const staffUsername = document.getElementById("stUsername").value
